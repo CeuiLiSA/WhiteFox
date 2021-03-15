2f0f6228bc56931fbc7bfd6a6a83f8a89ee2d232
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2021-03-15 14:31:15 +0800

    Initial commit

---
+++ app/src/main/java/ceui/lisa/whitefox/MainActivity.kt
@@ -0,0 +1,11 @@
+package ceui.lisa.whitefox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
