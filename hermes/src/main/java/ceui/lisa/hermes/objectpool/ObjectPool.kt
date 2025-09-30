ed6017c27947224f91f3e9c2d0f60e0a35549c40
SoxiaLiSA <ceuilisa@outlook.com>
2025-09-30 17:53:46 +0800

    ObjectPool

--- hermes/src/main/java/ceui/lisa/hermes/objectpool/ObjectPool.kt
+++ hermes/src/main/java/ceui/lisa/hermes/objectpool/ObjectPool.kt
@@ -3,26 +3,45 @@ package ceui.lisa.hermes.objectpool
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ceui.lisa.models.ModelObject
+import kotlin.reflect.KClass
 
 object ObjectPool {
 
-    val map = hashMapOf<ObjectKey, MutableLiveData<Any>>()
+    private val _map = hashMapOf<ObjectKey, MutableLiveData<Any>>()
 
+    inline fun <reified ObjectT : ModelObject> update(obj: ObjectT) {
+        update(obj, ObjectT::class)
+    }
 
-    fun <ObjectT : ModelObject> update(obj: ObjectT) {
-        val key = ObjectKey(obj.objectUniqueId, obj::class)
-        val liveData = map.getOrPut(key) {
-            MutableLiveData()
+    fun <ObjectT : ModelObject> update(
+        obj: ObjectT,
+        objCls: KClass<ObjectT>,
+    ) {
+        val key = ObjectKey(obj.objectUniqueId, objCls)
+        val record = getMutableRecord(key)
+        try {
+            record.value = obj
+        } catch (ex: Exception) {
+            throw RuntimeException(ex)
         }
-        liveData.value = obj
     }
 
     inline fun <reified ObjectT : ModelObject> get(id: Long): LiveData<ObjectT> {
-        val key = ObjectKey(id, ObjectT::class)
-        val liveData = map.getOrPut(key) {
+        return get(id, ObjectT::class)
+    }
+
+    fun <ObjectT : ModelObject> get(
+        objectId: Long,
+        objCls: KClass<ObjectT>,
+    ): LiveData<ObjectT> {
+        val key = ObjectKey(objectId, objCls)
+        return getMutableRecord(key) as LiveData<ObjectT>
+    }
+
+    private fun getMutableRecord(key: ObjectKey): MutableLiveData<Any> {
+        return _map.getOrPut(key) {
             MutableLiveData()
         }
-        return liveData as LiveData<ObjectT>
     }
 
 }
